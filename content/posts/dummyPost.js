1e92f6e498fdacb7fd977b219aad5e745d9e5282
David McKissick <dwm0816@outlook.com>
2018-06-08 18:42:04 -0700

    [change]added dummy files to utilize while developing systems. 1. Media folder 2. 2 template files for written content 3. dummy cover image

---
+++ content/posts/dummyPost.js
@@ -0,0 +1,10 @@
+let title = "Text between doublequotes"; //Title goes here
+let author = "My name!"; //Name of the author or group goes here
+let content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nibh arcu, fringilla nec urna at, porttitor rutrum urna. Maecenas faucibus tellus eget nunc commodo vestibulum. Aliquam quis eros accumsan, pulvinar turpis venenatis, cursus massa. Cras vel aliquam odio, eget pellentesque metus. Sed et ex suscipit, ultricies risus lacinia, cursus arcu. Praesent vitae ligula eget justo feugiat porttitor ac nec nunc. Proin congue, erat at mattis porttitor, tellus turpis vulputate nulla, non feugiat ante ante ut odio. In ut venenatis tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer dignissim imperdiet enim at tempor. Phasellus interdum volutpat ex, ut pulvinar lorem accumsan et. Donec tempus odio molestie, consectetur eros sit amet, lacinia lorem. Etiam commodo vel erat ac euismod. Phasellus sodales magna nec mi tincidunt pellentesque. Sed aliquam ex ac tortor ornare vulputate. Praesent et augue ut ex auctor tincidunt."; //Text content goes here
+let meta = {
+  contributor: "Jerry jenkins-jones",//This is used to add attribution to a co-author.
+  tag : ["lorem", "ipsum", "dolor", "sit", "amet"],//This is used to set tags for the article.
+  coverImg : "demo.jpeg"//This is used to set a cover image on themes that support it.
+};
+
+//Read the documentation (if there is one when you're reading this) to find a list of meta tags and how to use them.
---
+++ content/posts/post template.js
@@ -0,0 +1,10 @@
+//This is an ultra simple template made for first time users. For more complex edits, check out the documentation at URL_HERE.
+let title = "John does tater tots"; //Title goes here
+let author = "John doe"; //Name of the author or group goes here
+let content = "<your written content here>"; //Text content goes here
+let meta = {
+  tag : ["lorem", "ipsum", "dolor", "sit", "amet"],//This is used to set tags for the article.
+  coverImg : "demo.jpg"//This is used to set a cover image on themes that support it.
+};
+
+//Read the documentation (if there is one when you're reading this) to find a list of meta tags and how to use them.
